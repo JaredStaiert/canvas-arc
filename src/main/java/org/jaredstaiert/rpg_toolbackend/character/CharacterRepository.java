0370277189affb90b579e7ca64ee0e1bba70a1d7
jaredstaiert <jwstaiert@gmail.com>
2025-07-27 17:14:04 -0500

    Implemented native query for user summary

--- src/main/java/org/jaredstaiert/rpg_toolbackend/character/CharacterRepository.java
+++ src/main/java/org/jaredstaiert/rpg_toolbackend/character/CharacterRepository.java
@@ -1,11 +1,27 @@
 package org.jaredstaiert.rpg_toolbackend.character;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface CharacterRepository extends JpaRepository<Character, Integer> {
+    @Query(value = """
+                SELECT c.character_id,
+                       u.user_name,
+                       c.character_name,
+                       c.character_age,
+                       c.character_bio,
+                       w.world_name,
+                       t.timeline_name
+                FROM characters c
+                         LEFT JOIN users u ON c.user_id = u.user_id
+                         LEFT JOIN timelinecharacters tc ON c.character_id = tc.character_id
+                         LEFT JOIN timelines t ON tc.timeline_id = t.timeline_id
+                         LEFT JOIN worldcharacters wc ON c.character_id = wc.character_id
+                         LEFT JOIN worlds w ON wc.world_id = w.world_id;
+            """, nativeQuery = true)
     List<CharacterSummary> findAllProjectedBy();
 }
