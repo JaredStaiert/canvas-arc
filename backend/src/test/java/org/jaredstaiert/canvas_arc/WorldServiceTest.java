019e99be6da2d84c7a6d5918059733b43e872f61
jaredstaiert <jwstaiert@gmail.com>
2025-08-22 10:27:35 -0500

    Implementing unit tests :WorldService

--- backend/src/test/java/org/jaredstaiert/canvas_arc/WorldServiceTest.java
+++ backend/src/test/java/org/jaredstaiert/canvas_arc/WorldServiceTest.java
@@ -1,6 +1,7 @@
 package org.jaredstaiert.canvas_arc;
 
 import org.jaredstaiert.canvas_arc.world.World;
+import org.jaredstaiert.canvas_arc.world.WorldDTO;
 import org.jaredstaiert.canvas_arc.world.WorldRepository;
 import org.jaredstaiert.canvas_arc.world.WorldService;
 import org.junit.jupiter.api.AfterEach;
@@ -11,8 +12,12 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.mockito.Mockito.*;
 
 public class WorldServiceTest {
@@ -35,19 +40,88 @@ public class WorldServiceTest {
     }
 
     @Test
-    void testFindWorldById_returnsWorld() {
+    void testGetWorldById_returnsDTO() {
         // Arrange
-        int worldId = 1;
-        World mockWorld = new World("", "", LocalDate.now(), "");
-        mockWorld.setWorldID(worldId);
-        when(worldRepository.findById(worldId)).thenReturn(java.util.Optional.of(mockWorld));
+        int testWorldId = 1;
+
+        World mockWorld = new World();
+        mockWorld.setWorldID(testWorldId);
+        when(worldRepository.findById(testWorldId))
+                .thenReturn(java.util.Optional.of(mockWorld));
 
         // Act
-        World result = worldService.getWorldById(worldId);
+        WorldDTO result = worldService.getWorldById(testWorldId);
 
         // Assert
         assertThat(result).isNotNull();
-        assertThat(result.getWorldID()).isEqualTo(worldId);
-        verify(worldRepository).findById(worldId);
+        assertInstanceOf(WorldDTO.class, result);
+        verify(worldRepository).findById(testWorldId);
     }
+
+    @Test
+    void testGetWorldById_returnsCorrectWorld() {
+        // Arrange
+        int testWorldId = 1;
+        LocalDate testDate = LocalDate.of(2025, 8, 22);
+
+        World mockWorld = new World("name", "world", testDate, "desc");
+        mockWorld.setWorldID(testWorldId);
+
+        when(worldRepository.findById(testWorldId))
+                .thenReturn(java.util.Optional.of(mockWorld));
+
+        WorldDTO baselineDTO = new WorldDTO(
+                testWorldId,
+                "name",
+                "world",
+                testDate,
+                "desc");
+
+        // Act
+        WorldDTO result = worldService.getWorldById(testWorldId);
+
+        // Assert
+        assertThat(result).isNotNull();
+        assertEquals(result, baselineDTO, "DTO should contain same data as Entity");
+        verify(worldRepository).findById(testWorldId);
+    }
+
+
+    @Test
+    void testGetAllWorlds_returnsTypeList() {
+        int id1 = 1;
+        int id2 = 2;
+
+        World mockWorld1 = new World();
+        mockWorld1.setWorldID(id1);
+        World mockWorld2 = new World();
+        mockWorld2.setWorldID(id2);
+        //TODO: Write test after return type of WorldService.getAllWorlds is changed to WorldDTO
+    }
+
+    @Test
+    void testGetAllWorlds_returnsListDTO() {
+        //Arrange
+
+    }
+
+    @Test
+    void testGetAllWorlds_returnsCorrectWorlds() {
+
+    }
+
+//    @Test
+//    void testCreateWorld_savesWorld() {
+//        // Arrange
+//        WorldDTO newWorld = new WorldDTO(1,"", "World Name", LocalDate.now(), "");
+//
+//        when(worldRepository.save(any(World.class))).thenReturn(newWorld);
+//
+//        // Act
+//        World result = worldService.saveWorld(newWorld);
+//
+//        // Assert
+//        assertThat(result.getName()).isEqualTo("Arcane Realm");
+//        verify(worldRepository).save(newWorld);
+//    }
 }
