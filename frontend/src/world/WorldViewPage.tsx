3207867abecf3cea5334d13e1c5719c2239623b7
jaredstaiert <jwstaiert@gmail.com>
2025-09-08 12:17:45 -0500

    formatted

--- frontend/src/world/WorldViewPage.tsx
+++ frontend/src/world/WorldViewPage.tsx
@@ -1,34 +1,31 @@
-import { useParams } from "react-router-dom";
-import { getWorldToEditById } from "@/api/world_api";
-import { useQuery } from "@tanstack/react-query";
-import { Flex, Tabs } from "@mantine/core";
-import { useState } from "react";
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+import { Flex, Tabs } from '@mantine/core';
+import { getWorldToEditById } from '@/api/world_api';
 
 function WorldViewPage() {
-    const { worldID } = useParams();
+  const { worldID } = useParams();
 
-    const [activeTab, setActiveTab] = useState<string | null>('first');
+  const [activeTab, setActiveTab] = useState<string | null>('first');
 
-    const query = useQuery({ queryKey: ["worldByID", { worldID }], queryFn: getWorldToEditById });
+  const query = useQuery({ queryKey: ['worldByID', { worldID }], queryFn: getWorldToEditById });
 
-    return (
-        <>
-            <Flex
-                justify="flex-start"
-            >
-                <Tabs value={activeTab} onChange={setActiveTab}>
-                    <Tabs.List>
-                        <Tabs.Tab value="first">First tab</Tabs.Tab>
-                        <Tabs.Tab value="second">Second tab</Tabs.Tab>
-                    </Tabs.List>
+  return (
+    <>
+      <Flex justify="flex-start">
+        <Tabs value={activeTab} onChange={setActiveTab}>
+          <Tabs.List>
+            <Tabs.Tab value="first">First tab</Tabs.Tab>
+            <Tabs.Tab value="second">Second tab</Tabs.Tab>
+          </Tabs.List>
 
-                    <Tabs.Panel value="first">First panel</Tabs.Panel>
-                    <Tabs.Panel value="second">Second panel</Tabs.Panel>
-                </Tabs>
-
-            </Flex>
-        </>
-    );
+          <Tabs.Panel value="first">First panel</Tabs.Panel>
+          <Tabs.Panel value="second">Second panel</Tabs.Panel>
+        </Tabs>
+      </Flex>
+    </>
+  );
 }
 
 export default WorldViewPage;
