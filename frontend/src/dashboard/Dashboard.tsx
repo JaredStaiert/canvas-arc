35f8909e19ec28d744129ff5c6d3775caf6d8d3a
jaredstaiert <jwstaiert@gmail.com>
2025-09-22 12:07:10 -0500

    Displayed worlds as column list

--- frontend/src/dashboard/Dashboard.tsx
+++ frontend/src/dashboard/Dashboard.tsx
@@ -1,51 +1,63 @@
+import { JSX } from "react";
 import { useQuery } from "@tanstack/react-query";
-import { Badge, Button, Card, Flex, Group, Image, Text } from "@mantine/core";
-import { getWorlds } from "@/api/world_api";
+import { Badge, Box, Button, Grid, Group, Image, Paper, Stack, Text, Title } from "@mantine/core";
+import { getWorlds, World } from "@/api/world_api";
+
 
 function Dashboard() {
+  return (
+    <>
+      <Stack gap="xl" justify="flex-start">
+        <DashboardWorldList />
+      </Stack>
+    </>
+  );
+}
+
+function DashboardWorldList(): JSX.Element {
   const query = useQuery({ queryKey: ["all-worlds"], queryFn: getWorlds });
 
   if (query.isLoading) {
     return <div>Loading...</div>;
   }
 
-  return (
-    <>
-      <Flex direction="row" justify="flex-start" gap="xl">
-        <Flex mih={50} gap="sm" justify="center" align="center" direction="row" wrap="wrap">
-          {query.data?.map((world) => (
-            <Card
-              shadow="sm"
-              padding="lg"
-              radius="md"
-              withBorder
-              key={`${world.worldName}_dashboard`}
-              // mih="25em"
-              miw="25em"
-            >
-              <Card.Section>
+  function worldElementMap() {
+    return query.data?.map((world: World): JSX.Element => (
+      <Grid.Col span={12}>
+        <Paper shadow="sm" withBorder p="1rem">
+          <Stack>
+            <Title order={2}>{world.worldName}</Title>
+            <Group gap="xs" justify="flex-start">
+              <Box>
                 <Image
                   src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
-                  height={160}
+                  height="250rem"
+                  fit="contain"
                   alt="Norway"
+                  radius="sm"
                 />
-              </Card.Section>
-              <Group justify="space-between" mt="md" mb="xs">
-                <Text fw={500}>{world.worldName}</Text>
-                <Badge color="orange">GM</Badge>
-              </Group>
-
+              </Box>
+              <Badge color="orange">Owner</Badge>
+            </Group>
+            <Box mih="5rem">
+              <Title order={4}>World Description:</Title>
               <Text size="sm" c="dimmed">
                 {world.worldDesc}
               </Text>
+            </Box>
+            <Button color="green" mt="md" radius="md">
+              View {world.worldName}
+            </Button>
+          </Stack>
+        </Paper>
+      </Grid.Col>
+    ));
+  }
 
-              <Button color="blue" fullWidth mt="md" radius="md">
-                View {world.worldName}
-              </Button>
-            </Card>
-          ))}
-        </Flex>
-      </Flex>
+  return (
+    <>
+      <Title order={1}>Your Worlds</Title>
+      <Grid gutter="xl">{worldElementMap()}</Grid>
     </>
   );
 }
