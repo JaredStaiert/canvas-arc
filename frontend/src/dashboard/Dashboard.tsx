c246eb44a048759a62d259d8aeab1012db8b73ac
jaredstaiert <jwstaiert@gmail.com>
2025-09-22 12:19:57 -0500

    Moved old dashboard components to worlds page, will redesign dashboard

--- frontend/src/dashboard/Dashboard.tsx
+++ frontend/src/dashboard/Dashboard.tsx
@@ -7,61 +7,7 @@ import { getWorlds, World } from "@/api/world_api";
 function Dashboard() {
   return (
     <>
-      <Stack gap="xl" justify="flex-start">
-        <DashboardWorldList />
-      </Stack>
-    </>
-  );
-}
-
-function DashboardWorldList(): JSX.Element {
-  const query = useQuery({ queryKey: ["all-worlds"], queryFn: getWorlds });
-
-  if (query.isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  function worldElementMap() {
-    return query.data?.map(
-      (world: World): JSX.Element => (
-        <Grid.Col span={12}>
-          <Paper shadow="sm" withBorder p="1rem">
-            <Stack>
-              <Title order={2}>{world.worldName}</Title>
-              <Group gap="xs" justify="flex-start">
-                <Box>
-                  <Image
-                    src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
-                    height="250rem"
-                    fit="contain"
-                    alt="Norway"
-                    radius="sm"
-                  />
-                </Box>
-                <Badge color="orange">Owner</Badge>
-              </Group>
-              <Box mih="5rem">
-                <Title order={4}>World Description:</Title>
-                <Text size="sm" c="dimmed">
-                  {world.worldDesc}
-                </Text>
-              </Box>
-              <Link to={`/worlds/edit/${world.worldID}`} style={{ textDecoration: "none" }}>
-                <Button color="blue" fullWidth mt="md" radius="md">
-                  View {world.worldName}
-                </Button>
-              </Link>
-            </Stack>
-          </Paper>
-        </Grid.Col>
-      )
-    );
-  }
-
-  return (
-    <>
-      <Title order={1}>Your Worlds</Title>
-      <Grid gutter="xl">{worldElementMap()}</Grid>
+      <p>This is the dashboard</p>
     </>
   );
 }
--- frontend/src/world/WorldDashboardPage.tsx
+++ frontend/src/world/WorldDashboardPage.tsx
@@ -1,53 +1,80 @@
+import { JSX } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
-import { Badge, Button, Card, Flex, Group, Image, Paper, Text } from "@mantine/core";
-import { getWorlds } from "@/api/world_api";
+import {
+  Badge,
+  Box,
+  Button,
+  Card,
+  Flex,
+  Grid,
+  Group,
+  Image,
+  Paper,
+  Stack,
+  Text,
+  Title,
+} from "@mantine/core";
+import { getWorlds, World } from "@/api/world_api";
 
 function WorldDashboardPage() {
+  return (
+    <>
+      <Stack gap="xl" justify="flex-start">
+        <DashboardWorldList />
+      </Stack>
+    </>
+  );
+}
+
+function DashboardWorldList(): JSX.Element {
   const query = useQuery({ queryKey: ["all-worlds"], queryFn: getWorlds });
+
+  if (query.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  function worldElementMap() {
+    return query.data?.map(
+      (world: World): JSX.Element => (
+        <Grid.Col span={12}>
+          <Paper shadow="sm" withBorder p="1rem">
+            <Stack>
+              <Title order={2}>{world.worldName}</Title>
+              <Group gap="xs" justify="flex-start">
+                <Box>
+                  <Image
+                    src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
+                    height="250rem"
+                    fit="contain"
+                    alt="Norway"
+                    radius="sm"
+                  />
+                </Box>
+                <Badge color="orange">Owner</Badge>
+              </Group>
+              <Box mih="5rem">
+                <Title order={4}>World Description:</Title>
+                <Text size="sm" c="dimmed">
+                  {world.worldDesc}
+                </Text>
+              </Box>
+              <Link to={`/worlds/edit/${world.worldID}`} style={{ textDecoration: "none" }}>
+                <Button color="blue" fullWidth mt="md" radius="md">
+                  View {world.worldName}
+                </Button>
+              </Link>
+            </Stack>
+          </Paper>
+        </Grid.Col>
+      )
+    );
+  }
 
   return (
     <>
-      <Flex mih={50} gap="xl" justify="center" align="center" direction="row" wrap="wrap">
-        <Paper shadow="sm" radius="md" withBorder p="md">
-          <Button color="green" radius="md" size="lg">
-            New Setting
-          </Button>
-        </Paper>
-        {query.data?.map((world) => (
-          <Card
-            shadow="sm"
-            padding="lg"
-            radius="md"
-            withBorder
-            key={world.worldName}
-            // mih="25em"
-            miw="25em"
-          >
-            <Card.Section>
-              <Image
-                src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
-                height={160}
-                alt="Norway"
-              />
-            </Card.Section>
-            <Group justify="space-between" mt="md" mb="xs">
-              <Text fw={500}>{world.worldName}</Text>
-              <Badge color="orange">GM</Badge>
-            </Group>
-
-            <Text size="sm" c="dimmed">
-              {world.worldDesc}
-            </Text>
-
-            <Link to={`/worlds/edit/${world.worldID}`} style={{ textDecoration: "none" }}>
-              <Button color="blue" fullWidth mt="md" radius="md">
-                View {world.worldName}
-              </Button>
-            </Link>
-          </Card>
-        ))}
-      </Flex>
+      <Title order={1}>Your Worlds</Title>
+      <Grid gutter="xl">{worldElementMap()}</Grid>
     </>
   );
 }
