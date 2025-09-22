efb6e314188bb6f59feb2ffa3061f0effff07c13
jaredstaiert <jwstaiert@gmail.com>
2025-09-22 12:38:58 -0500

    sketched out dashboard display component

--- frontend/src/dashboard/Dashboard.tsx
+++ frontend/src/dashboard/Dashboard.tsx
@@ -1,7 +1,34 @@
+import { Grid, Paper, RingProgress, Stack, Title } from "@mantine/core";
+
 function Dashboard() {
   return (
     <>
-      <p>This is the dashboard</p>
+      <Grid>
+        <Grid.Col span={4}>
+          <Paper shadow="lg" withBorder>
+            <Stack>
+              <Title order={3}>Title</Title>
+                <DataTypeBreakdown/>
+            </Stack>
+          </Paper>
+        </Grid.Col>
+      </Grid>
+    </>
+  );
+}
+
+function DataTypeBreakdown() {
+  return (
+    <>
+      <RingProgress
+        size={200}
+        thickness={22}
+        sections={[
+          { value: 40, color: "cyan", tooltip: "Characters" },
+          { value: 15, color: "orange", tooltip: "Worlds" },
+          { value: 15, color: "grape", tooltip: "Timelines" },
+        ]}
+      />
     </>
   );
 }
