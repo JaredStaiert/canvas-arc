f3f58d5f181344f66a09f20ae94038621a2e1ab6
jaredstaiert <jwstaiert@gmail.com>
2025-09-22 12:09:02 -0500

    Linked main page button to worlds/edit endpoint

--- frontend/src/dashboard/Dashboard.tsx
+++ frontend/src/dashboard/Dashboard.tsx
@@ -1,8 +1,8 @@
 import { JSX } from "react";
 import { useQuery } from "@tanstack/react-query";
+import { Link } from "react-router-dom";
 import { Badge, Box, Button, Grid, Group, Image, Paper, Stack, Text, Title } from "@mantine/core";
 import { getWorlds, World } from "@/api/world_api";
-
 
 function Dashboard() {
   return (
@@ -22,36 +22,40 @@ function DashboardWorldList(): JSX.Element {
   }
 
   function worldElementMap() {
-    return query.data?.map((world: World): JSX.Element => (
-      <Grid.Col span={12}>
-        <Paper shadow="sm" withBorder p="1rem">
-          <Stack>
-            <Title order={2}>{world.worldName}</Title>
-            <Group gap="xs" justify="flex-start">
-              <Box>
-                <Image
-                  src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
-                  height="250rem"
-                  fit="contain"
-                  alt="Norway"
-                  radius="sm"
-                />
+    return query.data?.map(
+      (world: World): JSX.Element => (
+        <Grid.Col span={12}>
+          <Paper shadow="sm" withBorder p="1rem">
+            <Stack>
+              <Title order={2}>{world.worldName}</Title>
+              <Group gap="xs" justify="flex-start">
+                <Box>
+                  <Image
+                    src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
+                    height="250rem"
+                    fit="contain"
+                    alt="Norway"
+                    radius="sm"
+                  />
+                </Box>
+                <Badge color="orange">Owner</Badge>
+              </Group>
+              <Box mih="5rem">
+                <Title order={4}>World Description:</Title>
+                <Text size="sm" c="dimmed">
+                  {world.worldDesc}
+                </Text>
               </Box>
-              <Badge color="orange">Owner</Badge>
-            </Group>
-            <Box mih="5rem">
-              <Title order={4}>World Description:</Title>
-              <Text size="sm" c="dimmed">
-                {world.worldDesc}
-              </Text>
-            </Box>
-            <Button color="green" mt="md" radius="md">
-              View {world.worldName}
-            </Button>
-          </Stack>
-        </Paper>
-      </Grid.Col>
-    ));
+              <Link to={`/worlds/edit/${world.worldID}`} style={{ textDecoration: "none" }}>
+                <Button color="blue" fullWidth mt="md" radius="md">
+                  View {world.worldName}
+                </Button>
+              </Link>
+            </Stack>
+          </Paper>
+        </Grid.Col>
+      )
+    );
   }
 
   return (
