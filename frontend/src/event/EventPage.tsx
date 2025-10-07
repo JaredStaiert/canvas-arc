5e59b4cb7df88a396fc4762a44a0e485c4d9507b
jaredstaiert <jwstaiert@gmail.com>
2025-10-07 11:06:27 -0500

    added skeleton for event page table

--- frontend/src/event/EventPage.tsx
+++ frontend/src/event/EventPage.tsx
@@ -1,11 +1,54 @@
+import { JSX } from "react";
+import { Group, MultiSelect, Paper, Stack } from "@mantine/core";
 
 function EventPage() {
+  return (
+    <>
+      <Paper shadow="sm" radius="md">
+        <Stack justify="center" align="center">
+          <h1>All Events</h1>
+          <Group justify="center">
+            <MultiSelect
+              label="By World"
+              placeholder="World"
+              data={["React", "Angular", "Vue", "Svelte"]}
+              searchable
+              nothingFoundMessage="Nothing found..."
+              hidePickedOptions
+              clearable
+            />
+            <MultiSelect
+              label="By Name"
+              placeholder="Character Name"
+              data={["React", "Angular", "Vue", "Svelte"]}
+              searchable
+              nothingFoundMessage="Nothing found..."
+              hidePickedOptions
+              clearable
+            />
+            <MultiSelect
+              label="By Owner"
+              placeholder="Owner Name"
+              data={["React", "Angular", "Vue", "Svelte"]}
+              searchable
+              nothingFoundMessage="Nothing found..."
+              hidePickedOptions
+              clearable
+            />
+          </Group>
+          <EventTableComponent />
+        </Stack>
+      </Paper>
+    </>
+  );
+}
 
-    return (
-        <>
-            <p>This is the event page</p>
-        </>
-    );
+function EventTableComponent(): JSX.Element {
+  return (
+      <>
+          <h2>Here Table</h2>
+      </>
+  );
 }
 
 export default EventPage;
